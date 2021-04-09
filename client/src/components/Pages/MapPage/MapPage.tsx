abace7299c3bdec3868e3c71fad4b35e88027fd5
Alexander <shyguywannafly@gmail.com>
2021-04-09 14:18:10 +0300

    TypeScript

---
+++ client/src/components/Pages/MapPage/MapPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface MapPageProps {
+
+}
+
+export const MapPage: React.FC<MapPageProps> = ({}) => {
+    return (
+      <h3>Map Component</h3>
+    );
+}
---
+++ client/src/components/Pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface RegisterPageProps {
+
+}
+
+export const RegisterPage: React.FC<RegisterPageProps> = () => {
+  return (
+    <h3>Register Component</h3>
+  );
+}
